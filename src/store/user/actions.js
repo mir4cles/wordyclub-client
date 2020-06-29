e64735aea55a8f09da43f63d4518251659c3f5ec
mir4cles <jurimiralles@gmail.com>
2020-06-29 14:44:13 +0200

    second commit, small improvements on template

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -2,7 +2,7 @@ import { apiUrl } from "../../config/constants";
 
 import axios from "axios";
 
-import { selectToken, selectUser } from "./selectors";
+import { selectToken } from "./selectors";
 import {
   appLoading,
   appDoneLoading,
