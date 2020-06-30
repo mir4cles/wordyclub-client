47f7fba4bcaf29497ab1fc2c94380a1950c20c9c
mir4cles <jurimiralles@gmail.com>
2020-06-30 15:17:56 +0200

    api info hiddens in .env file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,5 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const wordsApiUrl = process.env.REACT_APP_WORDS_API_URL;
+export const wordsApiKey = process.env.REACT_APP_WORDS_API_KEY;
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
